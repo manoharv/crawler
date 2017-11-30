3ad293566294058da2311727c71cf99d5d9b001a
manoharv <manohar.vayyavuru@gmail.com>
2017-11-30 23:12:05 +0530

    Typo fix

--- src/main/java/com/searchiq/influencers/crawler/CrawlerApplicationBoot.java
+++ src/main/java/com/searchiq/influencers/crawler/CrawlerApplicationBoot.java
@@ -4,21 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Version;
 
-@EnableJpaRepositories
-@SpringBootApplication(scanBasePackages = { "com.searchiq.influencers" })
+@SpringBootApplication
 public class CrawlerApplicationBoot {
 	
-	@Value("facebook.secret")
-	private String fbSecret;
+	@Value("${facebook.secret}")
+	String fbSecret;
 
-	@Value("facebook.access.token")
-	private String fbAccessToken;
+	@Value("${facebook.access.token}")
+	String fbAccessToken;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlerApplicationBoot.class, args);
